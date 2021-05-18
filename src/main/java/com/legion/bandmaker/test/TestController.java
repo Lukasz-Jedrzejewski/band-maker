9b1f28e2269e53676db741375e5a3909bdf4313c
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-05-18 10:27:35 +0200

    Add basic configuration
    Add configuration for database connection.
    Add test controller.
    No configuration for liquibase yet.

---
+++ src/main/java/com/legion/bandmaker/test/TestController.java
@@ -0,0 +1,17 @@
+package com.legion.bandmaker.test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping()
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<>("Hello!", HttpStatus.OK);
+    }
+}
