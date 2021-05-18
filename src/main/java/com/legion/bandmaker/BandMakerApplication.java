1544a4c72d1a2a50b45303e2b35e3cc3e9c0e0b6
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-05-18 09:28:52 +0200

    initial commit

---
+++ src/main/java/com/legion/bandmaker/BandMakerApplication.java
@@ -0,0 +1,13 @@
+package com.legion.bandmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legion/bandmaker/BandMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legion.bandmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
