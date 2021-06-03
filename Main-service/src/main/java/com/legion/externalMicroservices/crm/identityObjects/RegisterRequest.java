16a0f0aea3a4834b73dc24305ecdc0060cf70087
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-06-03 11:46:36 +0200

    Add password validation
    Add custom password validator.

--- Main-service/src/main/java/com/legion/externalMicroservices/crm/identityObjects/RegisterRequest.java
+++ Main-service/src/main/java/com/legion/externalMicroservices/crm/identityObjects/RegisterRequest.java
@@ -1,17 +1,22 @@
 package com.legion.externalMicroservices.crm.identityObjects;
 
+import com.legion.externalMicroservices.crm.tools.validation.PasswordConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class RegisterRequest {
     @Email
+    @NotNull
     private String email;
+
+    @PasswordConstraint
     private String password;
     private UserType userType;
 }
---
+++ Main-service/src/main/java/com/legion/externalMicroservices/crm/tools/validation/PasswordConstraint.java
@@ -0,0 +1,16 @@
+package com.legion.externalMicroservices.crm.tools.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PasswordConstraint {
+    String message() default "Invalid password. Must consist of at least eight characters," +
+            " including lowercase letters, uppercase letters, numbers and special characters";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
