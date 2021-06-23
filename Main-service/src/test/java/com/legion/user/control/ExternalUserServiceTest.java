331853866b584088ac7ccb5934c2657c9ee6c16b
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-06-23 14:53:10 +0200

    Add unit tests for changePassword method in ExternalUserService

--- Main-service/src/test/java/com/legion/user/control/ExternalUserServiceTest.java
+++ Main-service/src/test/java/com/legion/user/control/ExternalUserServiceTest.java
@@ -1,13 +1,18 @@
 package com.legion.user.control;
 
 import com.legion.externalMicroservices.crm.control.CrmClient;
+import com.legion.externalMicroservices.crm.identityObjects.User;
+import com.legion.user.model.PasswordResetRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -50,5 +55,33 @@ class ExternalUserServiceTest {
 
         // then
         assertTrue(result);
+    }
+
+    @Test
+    void should_return_exception_message() {
+        // given
+        PasswordResetRequest request = new PasswordResetRequest("aaaaAAAA12@", "aaaAAA12@");
+
+        // when
+        ResponseEntity<?> result = userService.changePassword(request, UUID.randomUUID());
+
+        // then
+        assertEquals(HttpStatus.CONFLICT, result.getStatusCode());
+        assertEquals("The passwords don`t match", result.getBody());
+    }
+
+    @Test
+    void should_set_password_for_given_user_and_return_him() {
+        // given
+        PasswordResetRequest request = new PasswordResetRequest("aaaaAAAA12@", "aaaaAAAA12@");
+        User user = mock(User.class);
+        Mockito.when(crmClient.setPassword(Mockito.any(), Mockito.any())).thenReturn(ResponseEntity.ok(user));
+
+        // when
+        ResponseEntity<?> result = userService.changePassword(request, UUID.randomUUID());
+
+        // then
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals(user, result.getBody());
     }
 }
